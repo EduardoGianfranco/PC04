f5c2624aba34f7ad75bec1ce68a14864f4cc28b9
Eduardo Bustios <eduardo_bustios@usmp.pe>
2021-06-15 17:22:04 -0500

    Creado clase Fail

---
+++ Models/Fail.cs
@@ -0,0 +1,9 @@
+namespace PC04.Models
+{
+    public class Fail
+    {
+        public int Id { get; set; }
+        public string titulo { get; set; }
+        public int Usuario { get; set; }
+    }
+}
